fb4511afe74b69e0e98e79319f42707c14639c9c
sergi92 <45389112+sergi92@users.noreply.github.com>
2022-04-21 21:49:55 +0200

    badge

---
+++ my-svelte-project/src/components/Badge.svelte
@@ -0,0 +1,36 @@
+<script>
+    export let content="10% Off";
+</script>
+
+<span class="badge badge--primary">{content}</span>
+
+<style>
+    .badge{
+        border-radius: 20px;
+        font-size: 2rem;
+        font-weight: bold;
+        padding: 0.5rem 2rem;
+        white-space: nowrap;
+    }
+
+    .badge--primary{
+        background: var(--color-primary);
+        color: #fff;
+    }
+
+    .badge--secondary{
+        background: var(--color-secondary);
+        color: #fff;
+    }
+
+    .badge--small{
+        font-size: 1.6rem;
+
+    }
+
+    @media screen and (min-width: 1024px){
+        .badge{
+            font-size: 1.5rem;
+        }
+    }
+</style>
---
+++ my-svelte-project/src/components/Link.svelte
@@ -0,0 +1,32 @@
+<script>
+    export let content="Learn more";
+    export let href="#";
+</script>
+
+<a {href}>{content}</a>
+
+<style>
+a{
+    color: var(--color-accent);
+    text-transform: uppercase;
+    text-decoration: none;
+    font-size: 2rem;
+    font-weight: bold;
+}
+a::after{
+    content: '-->';
+    margin-left: 5px;
+    transition: margin 0.15s;
+}
+
+a:hover::after{
+    margin-left: 10px;
+
+}
+
+@media screen and (min-width: 1024px) {
+    a{
+        font-size: 1.5rem;
+    }
+}
+</style>
